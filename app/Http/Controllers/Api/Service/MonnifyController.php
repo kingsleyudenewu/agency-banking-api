f4c7e313e77b4c793f6eaaa59aa67b6375b9a225
Abah Joseph <joefazee@gmail.com>
2020-05-31 23:48:21 +0100

    fix message reference issue

--- app/Http/Controllers/Api/Service/MonnifyController.php
+++ app/Http/Controllers/Api/Service/MonnifyController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\Service;
 
+use App\Events\SendMessage;
 use App\Http\Controllers\APIBaseController;
 use App\Koloo\User;
+use App\Message;
 use App\ProvidusTransaction;
 use App\Services\Monnify\Api;
 use Illuminate\Http\Request;
@@ -21,6 +23,7 @@ class MonnifyController extends APIBaseController
     public function check(Request $request, Api $monnifyApi)
     {
         $logger = Log::channel('monnify-account-topup');
+
 
         $logger->info('Monnify webhook. Verifying a hash.', $request->all());
 
@@ -44,7 +47,7 @@ class MonnifyController extends APIBaseController
 
             $payment = $monnifyApi->getSuccessfulTransaction($request->get('transactionReference'));
 
-            if (! $user = User::findByProvidusReference($request->get('accountReference'))) {
+            if (! $user = User::findByProvidusReference($request->get('product')['reference'])) {
                 $logger->error('Monnify webhook. User account reference is not found. ', $request->all());
                 return $this->errorResponse('User not found.', null, 400);
             }
@@ -54,7 +57,18 @@ class MonnifyController extends APIBaseController
 
             ProvidusTransaction::create(['ref' => $payment->transactionReference, 'payload' => json_encode($payment), 'completed' => now()]);
 
+            $amountInfo = sprintf(' %s %s', $payment->currency, number_format($payment->payableAmount, 2)) . ' via Providus bank';
 
+            $message = Message::create([
+                'message' => sprintf(config('koloo.account_funded_message'), $amountInfo),
+                'message_type' => 'sms',
+                'user_id' => $user->getId(),
+                'sender' => $user->getId(),
+                'subject' => ''
+            ]);
+
+
+            event(new SendMessage($message, 'sms'));
 
         } catch(\Exception $exception) {
             $logger->error($exception->getMessage());
--- config/koloo.php
+++ config/koloo.php
@@ -5,5 +5,7 @@ return [
 
     'customer_welcome_sms' => "You are now saving with Koloo\n\n Your Koloo account number is: %s\n\nYour phone number is also a valid means of identification on our platform.",
 
-    "otp_message" => "Your Koloo OTP is: %s\n\nYour OTP is secret and must NOT be shared with anyone else."
+    "otp_message" => "Your Koloo OTP is: %s\n\nYour OTP is secret and must NOT be shared with anyone else.",
+
+    'account_funded_message' => 'Your Koloo account has been credited with: %s'
 ];
