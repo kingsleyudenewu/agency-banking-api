077c452339ecfd159dc9224e391f4dffc4094229
Abah Joseph <joefazee@gmail.com>
2020-06-06 20:46:53 +0100

    remove otp middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 
+use App\Http\Middleware\OTPRequired;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin-check' => \App\Http\Middleware\AdminCheck::class,
     ];
 }
