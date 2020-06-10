5fd1f1166c9d21a4f66a30158f8b2c26791ce144
Abah Joseph <joefazee@gmail.com>
2020-06-10 22:06:32 +0100

    add more fields to the account page

--- app/Http/Resources/AccountCollection.php
+++ app/Http/Resources/AccountCollection.php
@@ -20,18 +20,20 @@ class AccountCollection extends JsonResource
             'phone' => $this->user->phone,
             'name' => $this->user->name,
             'address' => $this->address,
-            'passport_photo_url' => '',
             'dob' => $this->dob,
             'gender' => $this->gender,
             'next_of_kin_name' => $this->next_of_kin_name,
             'next_of_kin_phone' => $this->next_of_kin_phone,
-            'agreement_form_url' => '',
             'created_at' => $this->created_at,
             'account_number' => $this->user->account_number,
             'country' => $this->user->country,
             'roles' => $this->user->roles,
-            'commission' => $this->commission / 100,
-            'providus_account_number' => $this->user->providus_account_number
+            'commission' => $this->commission,
+            'commission_for_agent' => $this->commission_for_agent,
+            'providus_account_number' => $this->user->providus_account_number,
+            'agreement_form_url' => $this->agreement_form_url,
+            'means_of_identification_url' => $this->means_of_identification_url,
+            'application_form_url' => $this->application_form_url
         ];
     }
 }
