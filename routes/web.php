59714c2a4e4df00e39e38b5729c3e17356995c06
Abah Joseph <joefazee@gmail.com>
2020-06-19 13:29:17 +0100

    remove unused type

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (\App\Services\Bitly\ShortUrl $url) {
+Route::get('/', function () {
     return redirect('https://koloo.ng');
 });
 
